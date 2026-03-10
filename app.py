971695056f29c7318d8864df1aeda24178efc171
Auwal <mohammedauwalhassan07@gmail.com>
2026-03-10 14:16:59 +0100

    -Create .flaskenv -Update .gitignore to ignore .flaskenv -Create forms in forms.py -Create app.py with basic Flask app structure

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from forms import RegisterForm, LoginForm
 
 # Initialize flask app
 app = Flask(__name__)
@@ -7,13 +8,20 @@ app = Flask(__name__)
 
 # Create a user model with id, email and password
 
-# Create a 
-
 # Create home route
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 # Create register route
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    return render_template("register.html", form=form)
 
 # Create login route
-
-
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template("login.html", form=form)
 
--- forms.py
+++ forms.py
@@ -3,5 +3,13 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 # Create Register Form
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField("Register")
 
 # Create Login Form
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
