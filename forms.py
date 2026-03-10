6664e01d19a9ae46c15a4b07a747311b1b0468b9
Auwal <mohammedauwalhassan07@gmail.com>
2026-03-10 13:51:34 +0100

    Create forms.py and update requirements.txt

---
+++ forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+# Create Register Form
+
+# Create Login Form
