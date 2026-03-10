c1a4dd0f9aab8c32bd87ceb191a80e0d038bd748
Auwal <mohammedauwalhassan07@gmail.com>
2026-03-10 13:43:49 +0100

    Add todo comments

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from flask import Flask, render_template
 
+# Initialize flask app
 app = Flask(__name__)
+
+# Create a database
+
+# Create a user model with id, email and password
+
+# Create a 
+
+# Create home route
+
+# Create register route
+
+# Create login route
+
+
+
