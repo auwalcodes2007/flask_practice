5eef1c14e3405950467713b791d68143f65dfd9a
Auwal <mohammedauwalhassan07@gmail.com>
2026-03-11 14:38:02 +0100

    add confirm password field to registration form

--- forms.py
+++ forms.py
@@ -1,12 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from models import db, User
 
 # Create Register Form
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    password2 = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Register")
+
+    # Validate if email already exists
+    def validate_email(self, email):
+        user = db.session.execute(db.select(User).where(User.email == email.data)).first()
+        if user:
+            raise ValidationError("Email already registered")
+
 
 # Create Login Form
 class LoginForm(FlaskForm):
